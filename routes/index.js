7947db1f3031fe3bfabef596b96092187bf1174b
nsoarim <99406537+nsoarim@users.noreply.github.com>
2022-04-16 22:10:14 +0900

    m

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const cors = require("cors");
+
+
+const movieRoute = require('./MovieRoute');
+const ratingsRoute = require('./RatingsRoute');
+const UserRouter = require('./UserRoute');
+const CommentRoute = require("./CommentRoute");
+const wantRoute = require('./WantRoute');
+const errorRoute = require('./ErrorRoute');
+
+
+router.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+router.use('/movie',
+ movieRoute);
+router.use('/rating', ratingsRoute);
+router.use('/want', wantRoute);
+router.use('/user', UserRouter);
+router.use("/comment", CommentRoute);
+router.use("/", errorRoute);
+
+module.exports = router;
